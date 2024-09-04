b3986a830bef2ff7bae266a3365d0c76f41689a1
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:38:18 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "LT",
-    heroTagline: "Lynn Tolbert Realty",
-    agentName: "Lynn Tolbert",
+    headerInitials: "CB",
+    heroTagline: "Chelle Bradley Realty",
+    agentName: "Chelle Bradley",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Lynn Tolbert was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Lynn Tolbert provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Lynn Tolbert were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Lynn Tolbert and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Lynn Tolbert is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Chelle Bradley was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Chelle Bradley provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Chelle Bradley were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Chelle Bradley and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Chelle Bradley is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "",
-    address: "Pelham, Alabama",
-    phone: "",
-    footerDescription: `Lynn Tolbert Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "michelleb@eraking.com",
+    address: "3075 John Hawkins Pkwy B Hoover, AL, US 35244",
+    phone: "205-721-0680",
+    footerDescription: `Chelle Bradley Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/lynn.tolbert.50/",
+        facebook: "https://www.facebook.com/profile.php?id=61560365262525",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "",
-        phone: "",
+        email: "michelleb@eraking.com",
+        phone: "205-721-0680",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/83090486_643365869800927_764826112256638976_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=53a332&_nc_ohc=YV-zw0lQpjcQ7kNvgHQ2htn&_nc_ht=scontent-sjc3-1.xx&oh=00_AYD8SgesfUQ6z9_SKdj95617OTQDI7HM71Ru7KBVG8XFqg&oe=66F993D2" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-1/454208339_122122510556345508_4320486710560945215_n.jpg?stp=dst-jpg_s200x200&_nc_cat=103&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=4XyQ4DCinXsQ7kNvgH8CoX2&_nc_ht=scontent-sjc3-1.xx&oh=00_AYAGsaA_ZYp-AXQJYGt_prqOlCwO4jnNwCTUCJONGuZE9g&oe=66DBAA8C" // Agent image URL
 };
 
 export default clientData;
